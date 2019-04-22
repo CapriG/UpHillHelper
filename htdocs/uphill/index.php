61a80a9549f82dbfacd2bdceeccd402c671bcdd8
CapriG <49106719+CapriG@users.noreply.github.com>
2019-04-22 13:02:04 -0600

    Update index.php

--- htdocs/uphill/index.php
+++ htdocs/uphill/index.php
@@ -1,12 +1,4 @@
 <?php
-    
-    // Create connection
-    //$con = mysqli_connect("127.0.0.1","hotspots","","Argus");
-    
-    // Check connection
-    //if (mysqli_connect_errno()) {
-    //    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    //}
 
     echo "<html><body>\n";
     
